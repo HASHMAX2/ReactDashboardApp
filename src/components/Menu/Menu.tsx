243eea55dc0a9448430c69182cd2c2a16bf0eb1d
Abdullah Hashmi <ali.hashmi0@gmail.com>
2023-08-10 01:44:28 +0530

    data mnupulation using the data.js file

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import "./menu.scss";
+import { Link } from "react-router-dom";
+import { menu } from "../../data";
 
 function Menu() {
   return (
-    <div>
-      menu
-    </div>
-  )
+    <>
+      <div className="menu">
+        {menu.map((e) => (
+          <div className="item" key={e.id}>
+            <span className="title">{e.title}</span>
+            {e.listItems.map((listItem) => (
+              <Link to="/" className="listItem" key={listItem.id}>
+                <img src={listItem.icon} alt="" />
+                <span className="listItemTitle">{listItem.title}</span>
+              </Link>
+            ))}
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default Menu
+export default Menu;
